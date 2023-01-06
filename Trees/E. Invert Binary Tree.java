f5a3d909518aeeb0c59f76e2d3ea5c5a1e9b1e82
varma-raviteja <122059749+varma-raviteja@users.noreply.github.com>
2023-01-05 19:32:44 -0500

    Update E. Invert Binary Tree.java

--- Trees/E. Invert Binary Tree.java
+++ Trees/E. Invert Binary Tree.java
@@ -1,4 +1,4 @@
-class Solution {
+class Solution { -O(n) O(h)
     public TreeNode invertTree(TreeNode root) {
         if(root==null)
         return null;
@@ -12,3 +12,23 @@ class Solution {
         return root;
         
     }
+                
+                
+// Level Order Traversal  - O(n) O(h)
+                
+class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) return null;
+        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            TreeNode current = queue.poll();
+            TreeNode temp = current.left;
+            current.left = current.right;
+            current.right = temp;
+            if (current.left != null) queue.add(current.left);
+            if (current.right != null) queue.add(current.right);
+        }
+        return root;
+    }
+}
