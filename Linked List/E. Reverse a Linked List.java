1d79639f419afae1d9adfdd18b67ff8b2c1c6232
varma-raviteja <122059749+varma-raviteja@users.noreply.github.com>
2023-01-07 16:07:36 -0500

    Create E. Reverse a Linked List.java

---
+++ Linked List/E. Reverse a Linked List.java
@@ -0,0 +1,30 @@
+// Iterative - O(n)
+
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null;  
+        ListNode current = head;
+    
+        
+        while(current != null) { 
+            ListNode next = current.next; 
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+       return prev; 
+    }
+}
+
+
+// Recursive
+
+class Solution {
+    public ListNode reverseList(ListNode head) {
+         if (head == null || head.next == null) return head;
+    ListNode p = reverseList(head.next);
+    head.next.next = head;
+    head.next = null;
+    return p;
+    }
+}
