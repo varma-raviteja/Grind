e735e1a70ca9b9790f766895fd8c2c2636f41f0c
varma-raviteja <122059749+varma-raviteja@users.noreply.github.com>
2023-01-08 18:14:19 -0500

    Create E. Subtree of Another tree.java

---
+++ Trees/E. Subtree of Another tree.java
@@ -0,0 +1,43 @@
+// do isSameTree while traversing every single node.
+
+public boolean isSubtree(TreeNode s, TreeNode t) { // takes O(m x n)
+  if (s == null) {
+    return t == null;
+  }
+  return isSame(s, t) || isSubtree(s.left, t) || isSubtree(s.right, t);
+}
+
+private boolean isSame(TreeNode t1, TreeNode t2) { // takes O(n)
+  if (t1 == null && t2 == null) return true;
+  if (t1 == null || t2 == null) return false;
+  
+  if (t1.val != t2.val) return false;
+  return isSame(t1.left, t2.left) && isSame(t1.right, t2.right);
+}
+
+
+// Preorder Sequencing - Make Preoder String and compare if the substree is substring on the parent string.
+
+
+public boolean isSubtree(TreeNode s, TreeNode t) {
+  // s
+  StringBuilder sb = new StringBuilder();
+  preorder(s, sb);
+  String seqS = sb.toString();
+  // t
+  sb = new StringBuilder();
+  preorder(t, sb);
+  String seqT = sb.toString();
+  
+  return seqS.indexOf(seqT) >= 0;
+}
+
+private void preorder(TreeNode t, StringBuilder sb) {
+  if (t == null) {
+    sb.append("null");
+    return;
+  }
+  sb.append("#" + t.val);
+  preorder(t.left, sb);
+  preorder(t.right, sb);
+}
