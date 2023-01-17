3461429b6a94180975a930b7130abb34cef13bbf
varma-raviteja <122059749+varma-raviteja@users.noreply.github.com>
2023-01-17 16:15:49 -0500

    Update E. Majority Element.java

--- EASY/Array/E. Majority Element.java
+++ EASY/Array/E. Majority Element.java
@@ -1,19 +1,5 @@
 // Brute force - O(N*N) O(1)
 
-class Solution {
-    public boolean containsDuplicate(int[] nums) {
-        HashSet<Integer> set =new HashSet<Integer>();
-        for(int i=0;i<nums.length;i++)
-        {
-            if(set.contains(nums[i]))
-            return true;
-            else
-            set.add(nums[i]);
-        }
-        return false;
-    }
-}
-
 // Sorting -O(NlogN) O(1)
 
 class Solution {
