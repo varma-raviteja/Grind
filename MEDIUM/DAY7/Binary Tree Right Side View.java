85d4329a78e18942de5dabfd785fccd4337d03a4
varma-raviteja <122059749+varma-raviteja@users.noreply.github.com>
2023-01-17 10:55:54 -0500

    Create Binary Tree Right Side View.java

---
+++ MEDIUM/DAY7/Binary Tree Right Side View.java
@@ -0,0 +1,46 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> res=new ArrayList<>();
+
+        if(root==null)
+        return res;
+
+        Queue<TreeNode> q=new LinkedList<>();
+        q.add(root);
+
+        while(!q.isEmpty())
+        {
+            int size=q.size();
+           
+            for(int i=0;i<size;i++)
+            {
+                TreeNode pNode= q.poll();
+                if(i==size-1)
+                res.add(pNode.val);
+                System.out.println();
+                if(pNode.left!=null)
+                q.add(pNode.left);
+                if(pNode.right!=null)
+                q.add(pNode.right);
+            }
+        }
+
+
+        return res;
+    }
+}
