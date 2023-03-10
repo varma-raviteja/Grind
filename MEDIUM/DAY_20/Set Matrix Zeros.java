8800c8bc990157780f1fb6daf3985dfee535b9de
varma-raviteja <122059749+varma-raviteja@users.noreply.github.com>
2023-03-10 17:02:51 -0500

    Create Set Matrix Zeros.java

---
+++ MEDIUM/DAY_20/Set Matrix Zeros.java
@@ -0,0 +1,113 @@
+class Solution {
+    public void setZeroes(int[][] matrix) {
+        Queue<Pair<Integer,Integer>> q = new LinkedList<>();
+
+        for(int i=0;i<matrix.length;i++)
+        {
+            for(int j=0;j<matrix[0].length;j++)
+            {
+                if(matrix[i][j]==0)
+                {
+                    q.add(new Pair<>(i,j));
+                }
+            }
+        }
+
+        while(!q.isEmpty())
+        {
+            Pair<Integer,Integer> p=q.poll();
+            int x=p.getKey();
+            int y=p.getValue();
+            for(int i=0;i<matrix[0].length;i++)
+                matrix[x][i]=0;
+            for(int i=0;i<matrix.length;i++)
+                matrix[i][y]=0;
+        }
+        
+    }
+}
+
+
+// Using a HasSet
+
+class Solution {
+  public void setZeroes(int[][] matrix) {
+    int R = matrix.length;
+    int C = matrix[0].length;
+    Set<Integer> rows = new HashSet<Integer>();
+    Set<Integer> cols = new HashSet<Integer>();
+
+    // Essentially, we mark the rows and columns that are to be made zero
+    for (int i = 0; i < R; i++) {
+      for (int j = 0; j < C; j++) {
+        if (matrix[i][j] == 0) {
+          rows.add(i);
+          cols.add(j);
+        }
+      }
+    }
+
+    // Iterate over the array once again and using the rows and cols sets, update the elements.
+    for (int i = 0; i < R; i++) {
+      for (int j = 0; j < C; j++) {
+        if (rows.contains(i) || cols.contains(j)) {
+          matrix[i][j] = 0;
+        }
+      }
+    }
+  }
+}
+
+ O(M*N) O(Max(matrx.length,matrix[0].length))
+
+// O(M*N)
+ 
+   class Solution {
+  public void setZeroes(int[][] matrix) {
+    Boolean isCol = false;
+    int R = matrix.length;
+    int C = matrix[0].length;
+
+    for (int i = 0; i < R; i++) {
+
+      // Since first cell for both first row and first column is the same i.e. matrix[0][0]
+      // We can use an additional variable for either the first row/column.
+      // For this solution we are using an additional variable for the first column
+      // and using matrix[0][0] for the first row.
+      if (matrix[i][0] == 0) {
+        isCol = true;
+      }
+
+      for (int j = 1; j < C; j++) {
+        // If an element is zero, we set the first element of the corresponding row and column to 0
+        if (matrix[i][j] == 0) {
+          matrix[0][j] = 0;
+          matrix[i][0] = 0;
+        }
+      }
+    }
+
+    // Iterate over the array once again and using the first row and first column, update the elements.
+    for (int i = 1; i < R; i++) {
+      for (int j = 1; j < C; j++) {
+        if (matrix[i][0] == 0 || matrix[0][j] == 0) {
+          matrix[i][j] = 0;
+        }
+      }
+    }
+
+    // See if the first row needs to be set to zero as well
+    if (matrix[0][0] == 0) {
+      for (int j = 0; j < C; j++) {
+        matrix[0][j] = 0;
+      }
+    }
+
+    // See if the first column needs to be set to zero as well
+    if (isCol) {
+      for (int i = 0; i < R; i++) {
+        matrix[i][0] = 0;
+      }
+    }
+  }
+}
