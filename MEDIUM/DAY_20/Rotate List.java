9365d90ecadc85ed4772199d0b60118a97337153
varma-raviteja <122059749+varma-raviteja@users.noreply.github.com>
2023-03-13 11:33:50 -0400

    Create Rotate List.java

---
+++ MEDIUM/DAY_20/Rotate List.java
@@ -0,0 +1,19 @@
+public ListNode rotateRight(ListNode head, int k) {
+    if(head == null || k == 0) {
+        return head;
+    }
+    ListNode p = head;
+    int len = 1;
+    while(p.next != null) {
+        p = p.next;
+        len++;
+    }
+    p.next = head;
+    k %= len;
+    for(int i = 0; i < len - k; i++) {
+        p = p.next;
+    }
+    head = p.next;
+    p.next = null;
+    return head;
+}
