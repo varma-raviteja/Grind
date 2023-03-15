b8970976875394cf9ad1ee89c02689b8813729fa
varma-raviteja <122059749+varma-raviteja@users.noreply.github.com>
2023-03-15 00:13:17 -0400

    Create Subarray with Sum k.java

---
+++ MEDIUM/DAY_15/Subarray with Sum k.java
@@ -0,0 +1,49 @@
+//O(n^3) O(1)
+
+public class Solution {
+    public int subarraySum(int[] nums, int k) {
+        int count = 0;
+        for (int start = 0; start < nums.length; start++) {
+            for (int end = start + 1; end <= nums.length; end++) {
+                int sum = 0;
+                for (int i = start; i < end; i++)
+                    sum += nums[i];
+                if (sum == k)
+                    count++;
+            }
+        }
+        return count;
+    }
+}
+
+//O(n^2) O(1)
+public class Solution {
+    public int subarraySum(int[] nums, int k) {
+        int count = 0;
+        for (int start = 0; start < nums.length; start++) {
+            int sum=0;
+            for (int end = start; end < nums.length; end++) {
+                sum+=nums[end];
+                if (sum == k)
+                    count++;
+            }
+        }
+        return count;
+    }
+}
+
+// O(n)  O(n)
+public class Solution {
+    public int subarraySum(int[] nums, int k) {
+        int count = 0, sum = 0;
+        HashMap < Integer, Integer > map = new HashMap < > ();
+        map.put(0, 1);
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+            if (map.containsKey(sum - k))
+                count += map.get(sum - k);
+            map.put(sum, map.getOrDefault(sum, 0) + 1);
+        }
+        return count;
+    }
+}
